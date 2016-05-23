b14b6a46aa5d09a2245c89f75190a9f7723b3b38
VitalinaLiane <vitalina.liane@gmail.com>
2016-05-23 16:34:06 +0300

    WithMigrating

--- db/migrate/20160506161700_add_sexes_to_contacts.rb
+++
@@ -1,8 +0,0 @@
-class AddSexesToContacts < ActiveRecord::Migration
-  def change
-  
-    add_column :contacts, :own_gender,        :string
-    add_column :contacts, :search_for_gender, :string
-    
-  end
-end
